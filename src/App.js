f9c46bdf47505260cefec853a0896cec452bd06e
NabidAlam <msa.nabid007@gmail.com>
2019-05-23 15:08:22 +0600

    props update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React from "react"
-import ContactCard from './ContactCard'
+import React from "react";
+import ContactCard from "./ContactCard";
+import Joke from "./Joke";
+
 function App() {
-    return (
-        <div className="contacts">
-        <ContactCard 
-            contact={{
-                name: "Mr. Whiskerson", 
-                imgUrl: "http://placekitten.com/300/200", 
-                phone: "(212) 555-1234", 
-                email: "mr.whiskaz@catnap.meow"}}
-        />
-        
-        <ContactCard 
-            contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
-        />
-        
-        <ContactCard
-            contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
-        />
-        
-        <ContactCard 
-            contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
-        />
-        
+  return (
+    <div>
+      <Joke
+        question="What's the best thing about Switzerland?"
+        punchLine="I don't know, but the flag is a big plus!"
+      />
+      <Joke
+        question="What's the best thing about ereeeeeeeeeeee?"
+        punchLine="I don't know, but the flag is a big jjjjjjjjjj!"
+      />
+      <Joke
+        question="What's the best thing about gggggggggg?"
+        punchLine="I don't know, but the flag is a big ffffffffff!"
+      />
+      <Joke
+        question="What's the best thing about ddddddddddddd?"
+        punchLine="I don't know, but the flag is a big sssssssssss!"
+      />
+      <Joke
+        question="What's the best thing about hhhhhhhhhhhhh?"
+        punchLine="I don't know, but the flag is a big bbbbbbbbb!"
+      />
     </div>
-    )
+  );
 }
 
-export default App
+export default App;
